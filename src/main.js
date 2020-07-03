3841462a6038f931bd78b239bc546c136b9a4b54
Beverly Potts <pottsbeverly@gmail.com>
2020-07-03 13:10:10 -0400

    Continually fail to access json object

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
+import { getExchange } from './exchange-service';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 
-$(document).ready(function() {
 
+async function getConversionRates() {
+  const apiResponse = await getExchange();
+  console.log(apiResponse);
+  if(!apiResponse) {
+    $(".result").html('<p>Something Went Wrong!/<p>');
+  } else {
+    return apiResponse;
+  }
+}
+
+//function convertCurrency(userDollars, selectedCurrency, conversionRates) {
+
+//}
+
+$(document).ready(function() {
+  $('#exchange-btn').click(function(event) {
+    event.preventDefault();
+    //const userDollars = parseInt($('#user-input').val());
+    //const selectedCurrency = $("#select-currency").val();
+    const conversionRates = getConversionRates();
+    alert(conversionRates);
+    
+   // convertCurrency(userDollars, selectedCurrency, conversionRates);
+  });
 });
