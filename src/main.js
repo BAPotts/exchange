cfd04af1cf44185fc3ae2d8d479fcd68edf7a293
Beverly Potts <pottsbeverly@gmail.com>
2020-07-03 15:51:05 -0400

    Add await code to main

--- src/main.js
+++ src/main.js
@@ -11,5 +11,25 @@ $(document).ready(function() {
     
     const userDollars = parseInt($('#user-input').val());
     const selectedCurrency = $("#select-currency").val();
-  });
-});
+
+    (async () => {
+      let exchangeService = new ExchangeService();
+      const response = await exchangeService.getJsonObject();
+      if(!response) {
+        $(".result").html('<h1>There has been an error processing your request</h1>')
+      } else {
+        getElements(response);
+      }
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      } else {
+        $('.showHumidity').text(`There was an error handling your request.`);
+        $('.showTemp').text(`Please check your inputs and try again!`);
+      }
+    }
+  })
+})
