2a6a3f94107b0a8d1cde0ec2a31b2c7863b09db9
Beverly Potts <pottsbeverly@gmail.com>
2020-07-03 17:21:41 -0400

    Fail to work

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   $('#exchange-btn').click(function(event) {
     event.preventDefault();
     
-    // const userDollars = parseInt($('#user-input').val());
+    const userDollars = parseInt($('#user-input').val());
     const selectedCurrency = $("#select-currency").val();
 
     (async () => {
@@ -18,18 +18,19 @@ $(document).ready(function() {
       if(!response) {
         $(".result").html('<h1>There has been an error processing your request</h1>');
       } else {
-        getElements(selectedCurrency);
+        getElements(selectedCurrency, response, userDollars);
       }
     })();
 
-    function getElements(selectedCurrency) {
-     let currencyArray = ['AED', 'ARS', 'AUD', 'BGN', 'BRL'];
-     for (let i = 0; i < currencyArray.length; i++){
-       if (selectedCurrency === currencyArray[i]) {
-         alert(selectedCurrency);
-       } else {
-         alert("NOPE");
-       }  
+    function getElements(selectedCurrency, response, userDollars) {
+      let currencyArray = ['AED', 'ARS', 'AUD', 'BGN', 'BRL'];
+      alert(selectedCurrency);
+      for (let i = 0; i < currencyArray.length; i++){
+        alert(currencyArray[i]);
+        if (selectedCurrency === currencyArray[i]) {
+         let exchangeValue = (response.conversion_rates.selectedCurrency * userDollars);
+          alert(exchangeValue);
+        }  
       }
     }
   });
