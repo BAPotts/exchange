d3673ac73ef37cd1bc7d1983746640bc9de3ce2b
Beverly Potts <pottsbeverly@gmail.com>
2020-07-03 15:10:49 -0400

    Blow it up and start over

--- src/main.js
+++ src/main.js
@@ -1,31 +1,15 @@
-import { getExchange } from './exchange-service';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 
 
-async function getConversionRates() {
-  const apiResponse = await getExchange();
-  if(!apiResponse) {
-    $(".result").html('<p>Something Went Wrong!/<p>');
-  } else {
-    $(".result").html(apiResponse.conversion_rates.USD);
-  }
-}
-
-//function convertCurrency(userDollars, selectedCurrency, conversionRates) {
-
-//}
 
 $(document).ready(function() {
   $('#exchange-btn').click(function(event) {
     event.preventDefault();
-    //const userDollars = parseInt($('#user-input').val());
-    //const selectedCurrency = $("#select-currency").val();
-    const conversionRates = getConversionRates();
-    alert(conversionRates);
     
-   // convertCurrency(userDollars, selectedCurrency, conversionRates);
+    const userDollars = parseInt($('#user-input').val());
+    const selectedCurrency = $("#select-currency").val();
   });
 });
