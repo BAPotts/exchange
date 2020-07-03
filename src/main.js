de3fa82b86bacfc17c4fdacaee3b187b267ff820
Beverly Potts <pottsbeverly@gmail.com>
2020-07-03 16:30:26 -0400

    Successfully access properties of JSON object

--- src/main.js
+++ src/main.js
@@ -2,34 +2,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-
+import { ExchangeService } from './exchange-service';
 
 
 $(document).ready(function() {
   $('#exchange-btn').click(function(event) {
     event.preventDefault();
     
-    const userDollars = parseInt($('#user-input').val());
-    const selectedCurrency = $("#select-currency").val();
+    // const userDollars = parseInt($('#user-input').val());
+    //const selectedCurrency = $("#select-currency").val();
 
     (async () => {
       let exchangeService = new ExchangeService();
       const response = await exchangeService.getJsonObject();
       if(!response) {
-        $(".result").html('<h1>There has been an error processing your request</h1>')
+        $(".result").html('<h1>There has been an error processing your request</h1>');
       } else {
         getElements(response);
       }
     })();
 
     function getElements(response) {
-      if (response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-      } else {
-        $('.showHumidity').text(`There was an error handling your request.`);
-        $('.showTemp').text(`Please check your inputs and try again!`);
-      }
+      $('.result').text(`The exchange rate is: ${response.conversion_rates.AED}`);
     }
-  })
-})
+  });
+});
